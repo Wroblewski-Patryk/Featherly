fd9a0231d905f7e753901732aaaedf72921d3d81
Patryk Wróblewski <wroblewskipatryk@gmail.com>
2026-03-05 18:10:03 +0100

    Admin: Fix Users page pagination and improve sidebar items

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::latest()->get();
+        $users = User::latest()->paginate(10);
 
         return Inertia::render('Admin/Users/Index', [
             'users' => $users
