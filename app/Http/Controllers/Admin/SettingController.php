365bff9d7b35affcbf2976e2ff1208f653bb26e1
Patryk Wróblewski <wroblewskipatryk@gmail.com>
2026-03-24 19:33:39 +0100

    SCL-016: add audit logging for RBAC and settings updates

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Page;
 use App\Models\Setting;
+use App\Support\AuditLogger;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -25,10 +26,18 @@ class SettingController extends Controller
     {
         // Don't save internal inertia inertia props
         $data = $request->except(['_method', '_token']);
+        $changedKeys = [];
 
         foreach ($data as $key => $value) {
             Setting::updateOrCreate(['key' => $key], ['value' => $value]);
+            $changedKeys[] = $key;
         }
+
+        AuditLogger::log('settings.updated', [
+            'changed_keys' => $changedKeys,
+            'changed_count' => count($changedKeys),
+        ]);
+
         return redirect()->back()->with('success', 'settings.update_success');
     }
 }
